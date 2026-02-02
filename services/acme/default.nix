329cc27f64d68eaa2fd069f41e2267bbc1573010
Polina Vishneva <poli@0iq.dev>
2026-02-03 00:30:19 +0100

    services: acme: update default email

--- services/acme/default.nix
+++ services/acme/default.nix
@@ -3,8 +3,10 @@
     acceptTerms = true;
 
     defaults = {
-      email = "poli0iq@disroot.org";
+      email = "poli@0iq.dev";
+
       dnsProvider = "spaceship";
+
       credentialFiles = {
         SPACESHIP_API_KEY_FILE = config.sops.secrets.spaceship_api_key.path;
         SPACESHIP_API_SECRET_FILE = config.sops.secrets.spaceship_api_secret.path;
